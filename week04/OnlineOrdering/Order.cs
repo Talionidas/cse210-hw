0d171b89873abf11aa760aef1fdeab0c8c025010
Your Name <talionidas@hotmail.com>
2026-03-28 22:03:03 +0100

    Online Ordering

---
+++ week04/OnlineOrdering/Order.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+
+public class Order
+{
+    private List<Product> _products = new List<Product>();
+    private Customer _customer;
+
+    public Order(Customer customer)
+    {
+        _customer = customer;
+    }
+
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
+
+    //public int GetProductCount()
+    //{
+        //return _products.Count;
+    //}
+
+    public double CalculateTotalCost()
+    {
+        double total = 0;
+
+        foreach (Product product in _products)
+        {
+            total += product.GetTotalCost();
+        }
+
+        if (_customer.LivesInUSA())
+        {
+            total += 5;
+        }
+        else
+        {
+            total += 35;
+        }
+
+        return total;
+    }
+
+    public string GetPackingLabel()
+    {
+        string label = "Packing Label:\n";
+
+        foreach (Product product in _products)
+        {
+            label += product.GetPackingLabel() + "\n";
+        }
+
+        return label;
+    }
+
+    public string GetShippingLabel()
+    {
+        return $"Shipping Label:\n{_customer.GetName()}\n{_customer.GetAddress()}";
+    }
+}
--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -1,9 +1,35 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the OnlineOrdering Project.");
+        Address address1 = new Address("21 Baker Str.", "New York", "NY", "USA");
+        Customer customer1 = new Customer("Arthur Doyle", address1);
+
+        Order order1 = new Order(customer1);
+        order1.AddProduct(new Product("Laptop", "LT-101", 1200, 1));
+        order1.AddProduct(new Product("Wireless-Mouse", "WM-102", 40, 2));
+        order1.AddProduct(new Product("Wireless-Keyboard", "WK-103", 85, 1));
+
+        Address address2 = new Address("Bahnhofstrasse 1", "Zurich", "ZH", "Switzerland");
+        Customer customer2 = new Customer("Anna Müller", address2);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(new Product("Portable-Phone", "PP-201", 950, 1));
+        order2.AddProduct(new Product("Phone-Charger", "PC-202", 25, 2));
+
+        Console.WriteLine("= ORDER 1 =");
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine($"Total Cost: ${order1.CalculateTotalCost()}");
+        Console.WriteLine();
+
+        Console.WriteLine("= ORDER 2 =");
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"Total Cost: ${order2.CalculateTotalCost()}");
     }
 }
+
+//no extra code
